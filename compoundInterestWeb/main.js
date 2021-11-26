c210cd6dbe07dabdc3606d390d51dd0dfec7e1cb
Carlo Occhiena <57464184+carloocchiena@users.noreply.github.com>
2021-11-26 16:45:38 +0100

    Add files via upload

---
+++ compoundInterestWeb/main.js
@@ -0,0 +1,25 @@
+
+
+function calculator() {
+
+    var initial = parseInt(document.getElementById("initial").value);
+    var recurring = parseInt(document.getElementById("recurring").value);
+    var interest = parseFloat(document.getElementById("interest").value);
+    var maturity = parseInt(document.getElementById("maturity").value);
+
+    var recurringAmount = 0;
+
+    capitalAmount = initial * (1 + interest) ** maturity;
+
+    maturity -- // this to evaluate the recurring amount at the end of each year
+
+    while (maturity > 0) {
+        var yearlyInterest = recurring * (1 + interest) ** maturity
+        maturity --
+        recurringAmount += yearlyInterest
+    }
+    
+    var total = capitalAmount + recurringAmount + recurring
+
+    result = document.getElementById("result").innerHTML = total.toFixed(2);
+}
---
+++ compoundInterestWeb/readme.md
@@ -0,0 +1,3 @@
+Function to calculate compound interest, inspired by a question I saw from an Instagram user.
+Checked against: https://economiapertutti.bancaditalia.it/calcolatori/calcolatore-dell-interesse/
+
